9834eaefe8e70db874fcc692e04e73c2c0d67c8a
octavio <3amsro@gmail.com>
2021-06-20 23:13:19 -0700

    La parte que faltaba de la traduccion de los casos de prueba

--- app/src/main/java/com/example/gg/Activity_Profile.kt
+++ app/src/main/java/com/example/gg/Activity_Profile.kt
@@ -13,53 +13,39 @@ import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity__profile.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.profile_toolbar.*
+import java.util.*
 
 class Activity_Profile : AppCompatActivity() {
-
    // private val currenUser = FirebaseAuth.getInstance().currentUser
    //Declaración variables
    private lateinit var txtUsename: EditText
     private lateinit var txtLastName: EditText
     private lateinit var txtEmail: EditText
-
     //  private lateinit var txtPassword: EditText
     //private lateinit var progressBar: ProgressBar
     var dbReference: DatabaseReference?=null
     var database: FirebaseDatabase?=null
     // private lateinit var auth: FirebaseAuth
-
-
     private lateinit var userID:String
     private val TAG = "Profile"
     lateinit var auth:FirebaseAuth
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__profile)
-
         back.setOnClickListener {
             onBackPressed()
         }
-
         database= FirebaseDatabase.getInstance() //Instancia para BD
         auth= FirebaseAuth.getInstance() //Instancia para la autenticación
         dbReference=database?.reference!!.child("User") //Referencia para leer o escribir en una ubicación en la BD
-
-
         val user=auth.currentUser
         val userreference=dbReference?.child(user?.uid!!)
-
         mostrar()
-
-
     }
     fun btnActualizar(view: View) //onClick
     {
         actualizar()
-
     }
-
     private fun mostrar()
     {
         val user=auth.currentUser
@@ -67,28 +53,20 @@ class Activity_Profile : AppCompatActivity() {
         txtUsename=findViewById(R.id.txtUsername)
         txtLastName=findViewById(R.id.txtLastName)
         txtEmail=findViewById(R.id.txtEmail)
-
-
         txtEmail.setText(user.email)
         userreference?.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 txtUsename.setText(snapshot.child("Nombre").value.toString())
                 txtLastName.setText(snapshot.child("Apellido").value.toString())
-
             }
-
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
         })
-
-
-
-
-
     }
     private fun actualizar()
     {
+        val idioma = Locale.getDefault().language
         val user=auth.currentUser
         val userreference=dbReference?.child(user?.uid!!)
         txtUsename=findViewById(R.id.txtUsername)
@@ -116,20 +94,31 @@ class Activity_Profile : AppCompatActivity() {
                     TODO("Not yet implemented")
                 }
             })
-
-            Toast.makeText(this, "Cambios guardados exitosamente.", Toast.LENGTH_LONG).show()
+            if (idioma == "es") {
+                Toast.makeText(this, "Cambios guardados exitosamente.", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Changes saved successfully", Toast.LENGTH_LONG).show()
+            }
 
         }
         else
         {
-            Toast.makeText(this, "¡Ups! Parece que no hemos podido guardar los cambios.", Toast.LENGTH_LONG).show()
-            if(TextUtils.isEmpty(username))
-            {
-                txtUsename.setError("Ingrese nombre")
-            }
-            if(TextUtils.isEmpty(lastname))
-            {
-                txtLastName.setError("Ingrese apellido")
+            if(idioma == "es") {
+                Toast.makeText(this, "¡Ups! Parece que no hemos podido guardar los cambios.", Toast.LENGTH_LONG).show()
+                if (TextUtils.isEmpty(username)) {
+                    txtUsename.setError("Ingrese nombre")
+                }
+                if (TextUtils.isEmpty(lastname)) {
+                    txtLastName.setError("Ingrese apellido")
+                }
+            }else{
+                Toast.makeText(this, "Ups! It seems that we have not been able to save the changes.", Toast.LENGTH_LONG).show()
+                if (TextUtils.isEmpty(username)) {
+                    txtUsename.setError("Enter name")
+                }
+                if (TextUtils.isEmpty(lastname)) {
+                    txtLastName.setError("Enter last name")
+                }
             }
         }
 
--- app/src/main/java/com/example/gg/activity_forgotPassword.kt
+++ app/src/main/java/com/example/gg/activity_forgotPassword.kt
@@ -10,6 +10,7 @@ import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import java.util.*
 
 class activity_forgotPassword : AppCompatActivity() {
 
@@ -27,41 +28,50 @@ class activity_forgotPassword : AppCompatActivity() {
     fun send(view:View)
     {
         val email=txtEmail.text.toString()
-
+        val idioma = Locale.getDefault().language
         if(!TextUtils.isEmpty(email))
         {
             auth.sendPasswordResetEmail(email).addOnCompleteListener(this){
                 task ->
 
                 if(task.isSuccessful){
-
-                    Toast.makeText(this, "Te he enviado un correo electrónico.", Toast.LENGTH_LONG).show()
+                    if(idioma == "es") {
+                        Toast.makeText(this, "Te he enviado un correo electrónico.", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this, "I have sent you an email", Toast.LENGTH_LONG).show()
+                    }
                 }
                 else //No se envia Email
                 {
-
-                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-                    {
-                        txtEmail.setError("Email no válido")
-                    }
-                    else if(auth.currentUser == null)
-                    {
-                        Toast.makeText(this, "Usuario no registrado.", Toast.LENGTH_LONG).show()
-                    }
-                    else
-                    {
-                        Toast.makeText(this, "¡Ups! No me es posible contactarme contigo.", Toast.LENGTH_LONG).show()
+                    if (idioma == "es") {
+                        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                            txtEmail.setError("Email no válido")
+                        } else if (auth.currentUser == null) {
+                            Toast.makeText(this, "Usuario no registrado.", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(this, "¡Ups! No me es posible contactarme contigo.", Toast.LENGTH_LONG).show()
+                        }
+                    }else{
+                        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                            txtEmail.setError("Invalidad Email")
+                        } else if (auth.currentUser == null) {
+                            Toast.makeText(this, "Unregistered user", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(this, "Ups! I cannot contact you.", Toast.LENGTH_LONG).show()
+                        }
                     }
                 }
             }
         }
         if(TextUtils.isEmpty(email))
         {
-            txtEmail.setError("Ingresar Email");
+            if(idioma == "es"){
+                txtEmail.setError("Ingresar Email");
+            }else{
+                txtEmail.setError("Enter Email");
+            }
         }
-
     }
-
     fun login(view: View) {
         startActivity(Intent(this,activity_login::class.java))
         finish()
